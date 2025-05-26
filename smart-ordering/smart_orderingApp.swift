1f8cf243ae558c0bc094bb6d9f2f948109923cfa
danghoanggeo <dang.hoang.geo@gmail.com>
2025-05-26 14:30:54 +0900

    Initial Commit

---
+++ smart-ordering/smart_orderingApp.swift
@@ -0,0 +1,17 @@
+//
+//  smart_orderingApp.swift
+//  smart-ordering
+//
+//  Created by Dang Hoang on 2025/05/26.
+//
+
+import SwiftUI
+
+@main
+struct smart_orderingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ smart-orderingTests/smart_orderingTests.swift
@@ -0,0 +1,17 @@
+//
+//  smart_orderingTests.swift
+//  smart-orderingTests
+//
+//  Created by Dang Hoang on 2025/05/26.
+//
+
+import Testing
+@testable import smart_ordering
+
+struct smart_orderingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
